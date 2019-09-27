b2f4f421f2e77a79e45ad38d758130b298708132
Adam Fowler <ajohnfowler@gmail.com>
2019-09-27 20:18:09 -0300

    Temp Checkin

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walkSpeed;
+
+    private Rigidbody2D rigidbody2D;
+    Vector2 direction;
+
+    private void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        direction = new Vector2(Input.GetAxis("Horizontal"), 0.0f);
+        direction *= walkSpeed;
+        direction.y = rigidbody2D.velocity.y;
+        rigidbody2D.velocity = direction;
+    }
+}
